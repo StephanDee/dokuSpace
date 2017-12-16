cd2a107715da4b7844d50f314e1dcf521b932ceb
Stephan D <duenkel.stephan@gmail.com>
2017-12-16 09:09:26 +0100

    devided cloud function and client side code into 2 diffrent folders.

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAFczu2NNTt32Rie_EKsmPFppJ4_tm_gmE",
-  authDomain: "dokuspace-67e76.firebaseapp.com",
-  databaseURL: "https://dokuspace-67e76.firebaseio.com",
-  projectId: "dokuspace-67e76",
-  storageBucket: "dokuspace-67e76.appspot.com",
-  messagingSenderId: "459513225648"
-};
