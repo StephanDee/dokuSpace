916dfb7ba7278863a12779c1862695ccec62d264
Stephan D <duenkel.stephan@gmail.com>
2018-01-11 16:14:22 +0100

    new Features: new cloud function added to convert images.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 'use strict';
+// firebase modules
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+// database reference
 const ref = admin.database().ref();
+
+// storage modules
+const gcs = require('@google-cloud/storage')({keyFileName: 'dokuspace-67e76-firebase-adminsdk-6c17m-d90ed717c0.json'});
+// provides methods to execute external programms
+const spawn = require('child-process-promise').spawn;
 
 exports.createProfile = functions.auth.user().onCreate(event => {
   // user data
@@ -34,6 +41,55 @@ exports.deleteProfile = functions.auth.user().onDelete(event => {
   // reference to database
   const profileRef = ref.child(`/profiles/${uid}`);
 
-  // set a new profile to database
+  // removes the specified profile from database
   return profileRef.remove();
+});
+
+exports.generateThumbnail = functions.storage.object().onChange(event => {
+  // file data
+  const object = event.data;
+  const filePath = object.name;
+  const fileName = filePath.split('/').pop();
+  const fileBucket = object.bucket;
+  const bucket = gcs.bucket(fileBucket);
+  const tempFilePath = `/tmp/${fileName}`;
+
+  // IMPORTANT to stop infinite loops
+  if(fileName.startsWith('thumb_')) {
+    console.log('Already a Thumbnail.');
+    return;
+  }
+
+  if(!object.contentType.startsWith('image/')) {
+    console.log('This is not an image.');
+    return;
+  }
+
+  if (object.resourceState === 'not_exists') {
+    console.log('This is a deletion event.');
+    return;
+  }
+
+  // download the image
+  return bucket.file(filePath).download({
+    destination: tempFilePath
+  }).then(() => {
+    // resize the image
+    console.log('Image downloaded locally to', tempFilePath);
+    // executes the imagemagick convert cli
+    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+  }).then(() => {
+    // write image to storage
+    console.log('Thumbnail created at', tempFilePath);
+    // this regex matches to the end of a string that contains a slash
+    // followed by zero or more or any character that is not a /slash
+    const thumbFilePath = filePath.replace(/(\/)?([^\/]*)$/,
+      '$1thumb_$2');
+
+    return bucket.upload(tempFilePath, {
+      destination: thumbFilePath
+    });
+  });
+
+
 });
